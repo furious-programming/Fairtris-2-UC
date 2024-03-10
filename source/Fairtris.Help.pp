55d7c3759e63c7fb1ea5e93ae3e7cd6fed997043
Jarosław Baran <furiousprogramming@gmail.com>
2024-03-10 17:12:28 +0100

    No longer supported scenes removed

--- source/Fairtris.Help.pp
+++ source/Fairtris.Help.pp
@@ -45,26 +45,18 @@ uses
 
 procedure THelpThread.Execute();
 var
-  Address: String = 'https://github.com/furious-programming/fairtris-2-uc/wiki';
+  Address: String = 'https://github.com/furious-programming/fairtris-2-uc/wiki/';
 begin
   case Logic.Scene.Current of
-    SCENE_MENU:            Address += '/prime-menu';
-    SCENE_MODES:           Address += '/game-modes';
-    SCENE_FREE_MARATHON:   Address += '/free-marathon';
-    SCENE_FREE_SPEEDRUN:   Address += '/free-speedrun';
-    SCENE_MARATHON_QUALS:  Address += '/marathon-qualifications';
-    SCENE_MARATHON_MATCH:  Address += '/marathon-match';
-    SCENE_SPEEDRUN_QUALS:  Address += '/speedrun-qualifications';
-    SCENE_SPEEDRUN_MATCH:  Address += '/speedrun-match';
-    SCENE_GAME_NORMAL:     Address += '/marathon';
-    SCENE_GAME_FLASH:      Address += '/marathon';
-    SCENE_SPEEDRUN_NORMAL: Address += '/speedrun';
-    SCENE_SPEEDRUN_FLASH:  Address += '/speedrun';
-    SCENE_PAUSE:           Address += '/game-pause';
-    SCENE_OPTIONS:         Address += '/game-options';
-    SCENE_KEYBOARD:        Address += '/set-up-keyboard';
-    SCENE_CONTROLLER:      Address += '/set-up-controller';
-    SCENE_TOP_OUT:         Address += IfThen(Memory.GameModes.IsMarathon, '/marathon-summary', '/speedrun-summary');
+    SCENE_MENU:        Address += 'prime-menu';
+    SCENE_LOBBY:       Address += 'lobby';
+    SCENE_GAME_NORMAL: Address += 'marathon';
+    SCENE_GAME_FLASH:  Address += 'marathon';
+    SCENE_PAUSE:       Address += 'game-pause';
+    SCENE_OPTIONS:     Address += 'game-options';
+    SCENE_KEYBOARD:    Address += 'set-up-keyboard';
+    SCENE_CONTROLLER:  Address += 'set-up-controller';
+    SCENE_TOP_OUT:     Address += 'summary';
   end;
 
   ShellExecute(0, 'open', PChar(Address), nil, nil, SW_SHOWNORMAL);
